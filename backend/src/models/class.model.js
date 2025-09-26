5fc147a3897e42d96f506e2b8eac9c34efeaf076
cgokulc <gokul281201@gmail.com>
2025-09-26 19:36:37 +0530

    backend: add generateAccessCode func to class model

---
+++ backend/src/models/class.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+const classSchema = new mongoose.Schema({
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Teacher",
+    required: true
+  },
+  accessCode: {
+    type: String
+  },
+  status: {
+      type: String,
+      enum: [ 'active', 'notActive']
+  },
+  {timestamps : true}
+);
+
+classSchema.methods.generateAccessCode= async makeid() {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 6; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+export const Query = mongoose.model(
+  'Query',
+  querySchema
+);
