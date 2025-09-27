a3942ce603b9370623a3be07dfee695faaffacd1
Sumit <sumitgk2003@gmail.com>
2025-09-27 13:36:58 +0530

    endpoint for joining the class

--- backend/src/routes/student.routes.js
+++ backend/src/routes/student.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerStudent,loginStudent,logoutStudent,createQuery,getCreatedQueries,getAllActiveClasses} from "../controllers/student.controller.js";
+import { registerStudent,loginStudent,logoutStudent,createQuery,getCreatedQueries,getAllActiveClasses,joinClass} from "../controllers/student.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router=Router() ;
@@ -12,5 +12,5 @@ router.route("/logout").post(verifyJWT,logoutStudent);
 router.route("/createQuery").post(verifyJWT,createQuery);
 router.route('/getCreatedQueries').get(verifyJWT,getCreatedQueries);
 router.route('/getAllActiveClasses').get(verifyJWT,getAllActiveClasses);
-
+router.route('/joinClass').post(verifyJWT,joinClass);
 export default router;
